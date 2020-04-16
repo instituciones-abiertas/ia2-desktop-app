dbc15370c71bfcf8f5b29ddf9c08e98ef06a1943
floreciendo <flor@RedMoon.flor>
2020-04-15 21:01:48 -0300

    add home screen component

--- app/components/Home.css.d.ts
+++ app/components/Home.css.d.ts
@@ -1,6 +1,11 @@
 declare const styles: {
+  readonly "bottomRow": string;
+  readonly "buttonsRow": string;
   readonly "container": string;
   readonly "content": string;
+  readonly "firstButton": string;
+  readonly "secondButton": string;
+  readonly "textRow": string;
 };
 export = styles;
 
--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,14 +1,33 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import routes from '../constants/routes.json';
+import { Button, Col, Row, Image } from 'react-bootstrap'; 
 import styles from './Home.css';
 
 export default function Home() {
   return (
     <div className={styles.container} data-tid="container">
       <div className={styles.content}></div>
-      <h2>LIBERAJUS</h2>
-      <h5>Anonimizamos para una justicia abierta</h5>
+        <Row><Image width="80px" src="./assets/documentIcon.png"/></Row>
+        <Row className={styles.textRow}>
+        </Row>
+          <h2>LIBERAJUS</h2>
+        <Row className={styles.textRow}>
+          <h5>Anonimizamos para una justicia abierta</h5>
+        </Row>
+        <Row className={styles.buttonsRow}>
+         <Col>
+           <Button variant="secondary" size="lg" className={styles.firstButton}>
+              ANÁLISIS
+          </Button>
+        </Col>
+        <Col>
+        <Button variant="secondary" size="lg" className={styles.secondButton}>
+          ANONIMIZAR
+        </Button>
+        </Col>
+        </Row>
+        <Row className={styles.bottomRow}>
+        <h6>Desarrollado por Cambá</h6>
+      </Row>
       </div>
   );
 }
