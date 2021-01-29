cf3be0ff6e75978c852a9f2e46e626196f3a2005
florencia <florencia@camba.coop>
2021-01-29 09:26:43 -0300

    Se modifica menu

--- app/menu.ts
+++ app/menu.ts
@@ -68,38 +68,6 @@ export default class MenuBuilder {
 
     const templateDefault = [
       {
-        label: '&Vista',
-        submenu:
-          process.env.NODE_ENV === 'development' ||
-          process.env.DEBUG_PROD === 'true'
-            ? [
-                {
-                  label: '&Reload',
-                  accelerator: 'Ctrl+R',
-                  click: () => {
-                    this.mainWindow.webContents.reload();
-                  },
-                },
-                {
-                  label: 'Toggle &Full Screen',
-                  accelerator: 'F11',
-                  click: () => {
-                    this.mainWindow.setFullScreen(
-                      !this.mainWindow.isFullScreen()
-                    );
-                  },
-                },
-                {
-                  label: 'Toggle &Developer Tools',
-                  accelerator: 'Alt+Ctrl+I',
-                  click: () => {
-                    this.mainWindow.webContents.toggleDevTools();
-                  },
-                },
-              ]
-            : [],
-      },
-      {
         label: 'Acerca de IA²',
         submenu: [
           {
