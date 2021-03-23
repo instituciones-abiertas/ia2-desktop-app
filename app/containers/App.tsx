78dd3e6a82606b01311d09da3b3c859dbacf5ab1
Santiago Botta <sgobotta@gmail.com>
2021-03-22 21:10:03 -0300

    :computer: Adds the application version at the bottom right of the screen

--- app/containers/App.tsx
+++ app/containers/App.tsx
@@ -1,6 +1,18 @@
 import React, { ReactNode } from 'react';
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
+import { Typography } from '@material-ui/core';
+import { version } from '../constants/misc'
+
+const Version = () => (
+  <Typography style={{
+    position: 'absolute',
+    bottom: '15px',
+    right: '8px',
+    color: '#6c757d',
+    cursor: 'default',
+  }}>{`v${version}`}</Typography>
+)
 
 type Props = {
   children: ReactNode;
@@ -45,5 +57,10 @@ export default function App(props: Props) {
   });
 
   const { children } = props;
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  return (
+    <ThemeProvider theme={theme}>
+      {children}
+      <Version />
+    </ThemeProvider>
+  )
 }
